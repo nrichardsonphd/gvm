bf63c534fc9f81134215d2b5c8fdb590676d11ef
nrichardsonphd <richardsonn@nsula.edu>
2015-06-04 12:34:06 -0500

    Keyword Token Literals

--- src/interpreter/Token_Literals.h
+++ src/interpreter/Token_Literals.h
@@ -1,6 +1,6 @@
 // This file contains the literals for each token
 
-// Single TOKENS
+// Single Tokens
 #define PLUS_TOK_LIT							'+'
 #define DASH_TOK_LIT							'-'
 #define SLASH_TOK_LIT							'/'
@@ -15,6 +15,7 @@
 #define SEMICOLON_TOK_LIT						';'
 
 #define APOSTROPHE_TOK_LIT						'\''
+#define QUOTE_TOK_LIT							'\"'
 #define TILDE_TOK_LIT							'~'
 #define LESS_TOK_LIT							'<'
 #define GREATER_TOK_LIT							'>'
@@ -24,10 +25,12 @@
 #define PIPE_TOK_LIT							'|'
 #define AMPERSAND_TOK_LIT						'&'
 #define COLON_TOK_LIT							':'
-#define CARROR_TOK_LIT							'^'
+#define CARROT_TOK_LIT							'^'
 #define BANG_TOK_LIT							'!'
+#define SPACE_TOK_LIT							' '
+#define TAB_TOK_LIT								'\t'
 
-// Paired tokens
+// Paired Tokens
 #define OPEN_PAREN_TOK_LIT						'('
 #define CLOSE_PAREN_TOK_LIT						')'
 #define OPEN_BRACKET_TOK_LIT					'['
@@ -68,4 +71,74 @@
 // Quadruple Tokens
 #define LESS_LESS_LESS_EQUAL_TOK_LIT			"<<<="
 #define GREATER_GREATER_GREATER_EQUAL_TOK_LIT	">>>="
+
+
+// Keyword Literals
+#define START_TOK_KW_LIT						"START"
+#define END_TOK_KW_LIT							"END"
+
+#define IF_TOK_KW_LIT							"IF"
+#define THEN_TOK_KW_LIT							"THEN"
+#define ELSE_TOK_KW_LIT							"ELSE"
+#define ENDIF_TOK_KW_LIT						"ENDIF"
+
+#define FOR_TOK_KW_LIT							"FOR"
+#define TO_TOK_KW_LIT							"TO"
+#define STEP_TOK_KW_LIT							"STEP"
+#define NEXT_TOK_KW_LIT							"NEXT"
+
+#define DO_TOK_KW_LIT							"DO"
+#define UNTIL_TOK_KW_LIT						"UNTIL"
+#define WHILE_TOK_KW_LIT						"WHILE"
+#define WEND_TOK_KW_LIT							"WEND"
+
+#define LET_TOK_KW_LIT							"LET"
+#define INT_TOK_KW_LIT							"INT"
+#define REAL_TOK_KW_LIT							"REAL"
+#define STRING_TOK_KW_LIT						"STRING"
+#define BOOL_TOK_KW_LIT							"BOOL"
+#define GRAPH_TOK_KW_LIT						"GRAPH"
+#define CLASS_TOK_KW_LIT						"CLASS"
+#define VERTEX_TOK_KW_LIT						"VERTEX"
+#define EDGE_TOK_KW_LIT							"EDGE"
+
+#define MOD_TOK_KW_LIT							"MOD"
+#define INFINITY_TOK_KW_LIT						"INFINITY"
+#define NULL_TOK_KW_LIT							"NULL"
+
+#define PRINT_TOK_KW_LIT						"PRINT"
+#define INPUT_TOK_KW_LIT						"INPUT"
+
+
+
+#define OPEN_TOK_KW_LIT							"OPEN"
+#define CLOSE_TOK_KW_LIT						"CLOSE"
+#define READ_TOK_KW_LIT							"READ"
+#define WRITE_TOK_KW_LIT						"WRITE"
+#define CLOSE_TOK_KW_LIT						"EOF"
+
+#define FUNCTION_TOK_KW_LIT						"FUNCTION"
+#define CALL_TOK_KW_LIT							"CALL"
+#define RETURN_TOK_KW_LIT						"RETURN"
+#define TRUE_TOK_KW_LIT							"TRUE"
+#define FALSE_TOK_KW_LIT						"FALSE"
+#define INCLUDE_TOK_KW_LIT						"INCLUDE"
+
+#define SET_TOK_KW_LIT							"SET"
+#define AND_TOK_KW_LIT							"AND"
+#define OR_TOK_KW_LIT							"OR"
+#define XOR_TOK_KW_LIT							"XOR"
+#define NOT_TOK_KW_LIT							"NOT"
+#define IN_TOK_KW_LIT							"IN"
+#define NOTIN_TOK_KW_LIT						"NOTIN"
+
+#define FORALL_TOK_KW_LIT						"FORALL"
+#define FA_TRUE_TOK_KW_LIT						"FA_TRUE"
+#define FA_FALSE_TOK_KW_LIT						"FA_FALSE"
+#define THERE_EXISTS_TOK_KW_LIT					"THERE_EXISTS"
+#define TE_TRUE_TOK_KW_LIT						"TE_TRUE"
+#define TE_FALSE_TOK_KW_LIT						"TE_FALSE"
+
+
+
 
