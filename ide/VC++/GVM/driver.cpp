29b943c2673f4e0054103e6ea928a019344385c8
nrichardsonphd <richardsonn@nsula.edu>
2015-06-04 13:33:51 -0500

    Start Screen
    Build number with hash, need to make this logical later

--- ide/VC++/GVM/driver.cpp
+++ ide/VC++/GVM/driver.cpp
@@ -1,10 +1,40 @@
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <string>
+#include <stdio.h>
 #include <iostream>
 using namespace std;
 
 #include "Scanner.h"
 
+void Get_Build_Number();
+
+
 int main()
 {
-	cout << PERCENT_TOK_LIT << endl;
+	cout << "Graph Virtual Machine\t";
+	Get_Build_Number();
+	cout << "Dr. Nicholas Richardson" << endl;
 	return 0;
+}
+
+
+void Get_Build_Number()
+{
+	FILE *fp;
+	char *build_number = new char[128];
+	
+
+	if ( fp = fopen( "./../../../.git/refs/heads/master", "r" ) )
+	{
+		fgets( build_number, 128, fp );
+		fclose( fp );
+	}
+	else
+	{
+		build_number = "000-00-0000";
+	}
+
+	
+	cout << "Build Number: " << build_number << endl;
 }
